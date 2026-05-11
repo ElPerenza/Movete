d120f45526132f7d2193ea060558a5a85506186f
MAX13666 <maxgamer13666@gmail.com>
2026-05-11 20:35:03 +0200

    Enabled CORS for frontend in develop

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,6 +15,11 @@ async function bootstrap() {
             forbidNonWhitelisted: true
         })
     );
+    app.enableCors({
+        origin: 'http://localhost:4200',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
     await app.listen(process.env["PORT"] ?? 3000);
 }
 bootstrap();
