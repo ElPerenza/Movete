2bdca3c4985bbed7c636b2e9f4000119c4c57bf5
MAX13666 <maxgamer13666@gmail.com>
2026-05-11 17:27:15 +0200

    Added global logger to the main app

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, ConsoleLogger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: new ConsoleLogger({
+            json: true,
+            logLevels: ['log']
+        }),
+    });
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
