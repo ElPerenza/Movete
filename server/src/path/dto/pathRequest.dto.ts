226e71962b9ec27a87c5c4b1247914af2693ebb9
MAX13666 <maxgamer13666@gmail.com>
2026-05-14 10:17:05 +0200

    Created the request class for path creation

--- server/src/path/dto/pathRequest.dto.ts
+++ server/src/path/dto/pathRequest.dto.ts
@@ -1,3 +1,34 @@
+
 export class PathRequestDto {
-    
+    from: Coordinates;
+    to: Coordinates;
+    dateTime: Date;
+    modes: Modes;
+    //this is false by default, if true use the dateTime as arriveTime and not departure time.
+    arriveBy: boolean;
+}
+
+const transportModes = ["air", "bus", "cableway", "coach", "funicolar", "lift", "metro", "monorail", "rail", "taxi", "tram", "trolleybus", "unknown", "water"] as const;
+type TransportModes = (typeof transportModes)[number];
+
+//TODO verify how to configure OTP to make it understaned that we can travel with bike on trains
+const accessMode = ["car_pickup", "foot"];
+type AccessMode = (typeof accessMode)[number];
+
+const egressMode = ["car_pickup", "foot"];
+type EgressMode = (typeof egressMode)[number];
+
+const directMode = ["car", "bicycle"]
+type DirectMode = (typeof directMode)[number];
+
+class Modes {
+    accessMode: AccessMode;
+    transportModes: TransportModes;
+    egressMode: EgressMode;
+    directMode: DirectMode;
+}
+
+class Coordinates {
+    //this must be lat lon
+    coordinates: [number, number];
 }
