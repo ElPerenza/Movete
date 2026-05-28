e6e227503e7a7a8b28eadb7c658dfdd44cdacfbb
ElPerenza <pev.cookie@gmail.com>
2026-05-28 02:01:40 +0200

    Always track trips that we know have realtime data available

--- server/src/realtime/gtfs-realtime-providers/trentino-trasporti-gtfs-realtime-provider.ts
+++ server/src/realtime/gtfs-realtime-providers/trentino-trasporti-gtfs-realtime-provider.ts
@@ -16,9 +16,11 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
 
     private readonly logger = new Logger(TrentinoTrasportiGtfsRealtimeProvider.name);
 
-    private tripDates?: TripDates[];
+    // TODO: A lot of shared state between methods in this class. It works and isn't terribly complex, but I'd like a cleaner way of doing things in the future
+    private tripDates?: TripDates[]; // these are all trips in the feed that this provider must consider. They don't change after initialization. Should they be passed in the constructor?
     private runningToday?: TripDepartureArrivalTimes[];
-    private feed?: Uint8Array
+    private readonly trackedTrips: Set<string> = new Set();
+    private feed?: Uint8Array;
 
     constructor(
         private readonly realtimeService: OtpRealtimeService,
@@ -34,7 +36,7 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
         // cronjob time is tied to specific timezones isn't it... not a problem as long as the server is running in the same timezone as the buses.
         const job = new CronJob("0 0 2 * * *", async () => {
             try {
-                await this.updateTripsRunningToday();
+                this.runningToday = await this.getTripsRunningToday();
             } catch(err) {
                 this.logger.error(err);
             }
@@ -61,17 +63,18 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
     }
 
     /**
-     * Populate `this.runningToday` with the arrival and departure times of trips running on the current date.
+     * Retrieve the arrival and departure times of trips running on the current date.
+     * @returns the arrival and departure times of trips running today
      */
-    private async updateTripsRunningToday() {
+    private async getTripsRunningToday(): Promise<TripDepartureArrivalTimes[]> {
 
         if(!this.tripDates) {
             this.tripDates = await this.realtimeService.getTripsDatesByFeed(this.feedId);
         }
 
-        const todayDate = this.realtimeService.formatAsYYYYMMDDD(new Date());
+        const todayDate = this.realtimeService.formatAsYYYYMMDDD(new Date()); // TODO: timezones? as long as server is running in Europe/Rome timezone it's fine
         const tripsRunning = this.tripDates.filter(td => td.activeDates.includes(todayDate));
-        this.runningToday = await Promise.all(tripsRunning.map(async td => {
+        return await Promise.all(tripsRunning.map(async td => {
             return await this.realtimeService.getTripDepartureArrivalTimes(td.tripId, todayDate);
         }));
     }
@@ -79,24 +82,21 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
     /**
      * Updates (or creates) the realtime feed referencing only trips that are currently running this instant.
      */
-    private async updateFeed() {
+    private async updateFeed(): Promise<void> {
         const start = Date.now();
 
         if(!this.runningToday) {
-            await this.updateTripsRunningToday();
+            this.runningToday = await this.getTripsRunningToday();
         }
 
         const now = Math.floor(Date.now() / 1000);
-        const tripsCurrentlyRunning = this.runningToday!
-            .filter(times => {
-                // TODO: this includes all trips that *should* be currently moving, but if a trip is late enough it will not be included.
-                //       Need to keep track of trips for which we know we have realtime data to keep updating them.
-                return now >= times.departureTime && now <= times.arrivalTime;
-            }).map(times => { 
-                return { tripId: times.tripId, serviceDate: times.serviceDate };
-            });
+        const tripsCurrentlyRunning = this.runningToday
+            .filter(times => now >= times.departureTime && now <= times.arrivalTime) // TODO: consider if extending the bounds for inclusion a few minutes before and after "now"
+            .map(times => times.tripId.substring(times.tripId.indexOf(":") + 1)); // remove feedId (OTP gtfs IDs -> feedId:tripId)
         
-        this.feed = await this.createTripUpdatesFeed(tripsCurrentlyRunning);
+        const trips = new Set(tripsCurrentlyRunning).union(this.trackedTrips);
+        this.feed = await this.createTripUpdatesFeed(trips);
+
         this.logger.log(`[${this.feedId}] Created feed in ${Date.now() - start}ms. Made ${tripsCurrentlyRunning.length} requests to the TT API.`)
     }
 
@@ -117,10 +117,11 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
 
     /**
      * Create a trip updates realtime feed for the given trips.
+     * Trip updates are always for the current day, as the TT API does not support searching for trips on days different than the current one.
      * @param trips the trips to include in the feed
      * @returns the already encoded feed
      */
-    private async createTripUpdatesFeed(trips: { tripId: string, serviceDate: string }[]): Promise<Uint8Array> {
+    private async createTripUpdatesFeed(trips: Iterable<string>): Promise<Uint8Array> {
 
         const realtimeFeed = create(FeedMessageSchema, {
             header: {
@@ -129,23 +130,26 @@ export class TrentinoTrasportiGtfsRealtimeProvider implements GtfsRealtimeProvid
                 timestamp: BigInt(Math.floor(Date.now() / 1000))
             }
         });
+        const todayDate = this.realtimeService.formatAsYYYYMMDDD(new Date());
 
-        for(const { tripId, serviceDate } of trips) {
-            const apiTripId = tripId.substring(tripId.indexOf(":") + 1); // remove feedId (OTP -> feedId:tripId)
-
-            const tripInfo = await this.ttApiService.getTripInfo(apiTripId);
+        for(const tripId of trips) {
+            const tripInfo = await this.ttApiService.getTripInfo(tripId);
             if(tripInfo.delay == null || tripInfo.stopLast == tripInfo.stopNext) {
-                // no realtime data
+                // no realtime data (either lost signal or trip completed)
+                this.trackedTrips.delete(tripId);
                 continue;
             }
 
+            // keep track of trips that have realtime info, so we don't stop considering them if they're late and go out of the window defined in updateFeed()
+            this.trackedTrips.add(tripId);
+
             realtimeFeed.entity.push(create(FeedEntitySchema, {
-                id: apiTripId,
+                id: tripId,
                 tripUpdate: {
                     timestamp: BigInt(Math.floor(Date.parse(tripInfo.lastEventRecivedAt) / 1000)),
                     trip: {
-                        tripId: apiTripId,
-                        startDate: serviceDate
+                        tripId: tripId,
+                        startDate: todayDate
                     },
                     stopTimeUpdate: [
                         {
