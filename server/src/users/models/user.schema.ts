dfe212b27c3d0f99fdc573cc66ae28d12ab3c431
r507 <riccardo.sottoriva@proton.me>
2026-05-31 17:54:27 +0200

    user.schema update to differentiate between normal users, admin and employees

--- server/src/users/models/user.schema.ts
+++ server/src/users/models/user.schema.ts
@@ -1,6 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
 
+export enum UserRole {
+    USER = 'user',
+    ADMIN = 'admin',
+    EMPLOYEE = 'employee',
+}
+
+
 @Schema()
 export class User {
     @Prop({ required: true, unique: true })
@@ -8,6 +15,9 @@ export class User {
 
     @Prop({ required: true })
     password: string; // Hash bcrypt
+
+    @Prop({ type: String, enum: UserRole, default: UserRole.USER })
+    role: UserRole;
 }
 
 export type UserDocument = HydratedDocument<User>;
